8cf2f49ca89ca97cd1b8ebed758565d31d0eab9b
unknown <lahiruhiranwijesooriyansbm@gmail.com>
2020-05-25 14:02:06 +0530

    add the function to do the excel work book tasks

--- excel_analyse.py
+++ excel_analyse.py
@@ -1,22 +1,24 @@
 import openpyxl as xl
 from openpyxl.chart import  BarChart, Reference
-wb = xl.load_workbook("price.xlsx")
 
-sheet = wb["Sheet1"]
-cell = sheet["a1"]
-cell = sheet.cell(1,3)
-print(cell.value)
+filename ="price.xlsx"
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
 
-for row in range(2,sheet.max_row+1):
-    cell=sheet.cell(row,3)
-    print(cell.value)
-    correct_price = cell.value *0.9
-    corrected_price_cell = sheet.cell(row,4)
-    corrected_price_cell.value =correct_price
+    sheet = wb["Sheet1"]
+    cell = sheet["a1"]
+   
 
-values = Reference(sheet,min_row=2,max_row=sheet.max_row,min_col=4,max_col=4)
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart,"G4")
+    for row in range(2,sheet.max_row+1):
+        cell=sheet.cell(row,3)
+        print(cell.value)
+        correct_price = cell.value *0.9
+        corrected_price_cell = sheet.cell(row,4)
+        corrected_price_cell.value =correct_price
 
-wb.save("price_update.xlsx")
+    values = Reference(sheet,min_row=2,max_row=sheet.max_row,min_col=4,max_col=4)
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart,"G4")
+
+    wb.save(filename)
